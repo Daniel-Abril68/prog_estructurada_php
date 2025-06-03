c077d093292ec9b2782f720b8f3124c963877280
Daniel_Abril68 <luisabril@colegioguanenta.edu.co>
2025-06-03 07:01:47 -0500

    se agregaron nuevos comentarios

--- 2_comentarios.php
+++ 2_comentarios.php
@@ -1,3 +1,7 @@
 <?php
-    //Este es un comenratio de una oa linea o renglon
-    /*este es un comentarrio 
+    // Este es un comentario de una sola linea o renglón
+    
+    /*Este es un comentario 
+    de varias lineas o renglones.
+    Sistemas Guanenta*/
+?>
